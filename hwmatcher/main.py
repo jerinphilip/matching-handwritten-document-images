6d7faa1638ec4eba45856e198693b521cd61ba6c
Jerin Philip <jerin.philip@research.iiit.ac.in>
2019-04-18 17:33:44 +0530

    Adding munkres

--- hwmatcher/main.py
+++ hwmatcher/main.py
@@ -4,7 +4,8 @@ import cv2
 from argparse import ArgumentParser
 from collections import namedtuple
 from recordtype import recordtype
-
+import munkres
+import numpy as np
 
 def add_paths():
     THIRD_PARTY_ROOT = 'third-party'
@@ -14,6 +15,11 @@ def add_paths():
 add_paths()
 from east import EASTWrapper
 from utils import HWNetInferenceEngine
+import torch
+from torch.nn.modules.distance import CosineSimilarity
+from munkres import Munkres
+
+
 
 class Comparator:
     def __init__(self, east_path, hwnet_path):
@@ -23,14 +29,49 @@ class Comparator:
     def __call__(self, image_A, image_B):
         Sample = recordtype('Sample', 'image bboxes features')
         def sample(image):
-            _image, unit_bboxes = self.eastw.predict(image_A)
+            _image, unit_bboxes = self.eastw.predict(image)
             return Sample(image=image, bboxes=unit_bboxes, features=None)
 
         a = sample(image_A)
         b = sample(image_B)
         a.features = self.hwnet(a)
         b.features = self.hwnet(b)
-        # print(len(a.bboxes), len(b.bboxes))
+        with torch.no_grad():
+            matrix = self.compute_cost_matrix(a.features, b.features)
+            matrix = 1 - matrix
+
+        matrix = matrix.tolist()
+        m = Munkres()
+        indexes = m.compute(matrix)
+        total = 0
+        for row, column in indexes:
+            value = matrix[row][column]
+            total += value
+            print(row, column, '->', value)
+            # print(f'({row}, {column}) -> {value}')
+        # print(f'total cost: {total}')
+        print('total cost', total)
+
+            
+
+    def compute_cost_matrix(self, A, B):
+        similarity = CosineSimilarity()
+        num_A, _ = A.size()
+        num_B, _ = B.size()
+        _matrix = np.zeros((num_A, num_B))
+        for i in range(num_A):
+            vA = A[i, :].unsqueeze(0)
+            vA = vA.cuda()
+            for j in range(num_B):
+                vB = B[j, :].unsqueeze(0)
+                vB = vB.cuda()
+                value = similarity(vA, vB)
+                _matrix[i, j] = value.item()
+        return _matrix
+
+
+
+
 
 
 if __name__ == '__main__':
